34c5495b836238cf1aaa7e60aef04aa41aa83269
Chamela Aluthgedara <chamelaaluthgedara12@gmail.com>
2020-03-14 18:32:17 +0530

    Add project files.

---
+++ ITPM_Code_Complexity_Tool/Controllers/FileUploadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITPM_Code_Complexity_Tool.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
