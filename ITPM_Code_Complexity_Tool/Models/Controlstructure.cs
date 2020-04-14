99beb7d5a76ae828b24b70e80af214f7add1746d
buddi <buddhi.gamlath@gmail.com>
2020-04-14 17:47:45 +0530

    Control structure Calculate completed part

---
+++ ITPM_Code_Complexity_Tool/Models/Controlstructure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITPM_Code_Complexity_Tool.Models
+{
+    public class Controlstructure
+    {
+        
+        public int  LineNO { get; set; }
+
+        public string ProgramStatment { get; set; }
+
+        public int Wtcs { get; set; }
+
+        public int NC { get; set; }
+
+        public int Ccpps { get; set; }
+
+        public int Ccs { get; set; }
+
+    }
+}
