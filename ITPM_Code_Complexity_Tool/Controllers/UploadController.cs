5e6442d81dd065752c93a7f4cb3ab5ab8263733f
Pasanrc <Pasanrc@DESKTOP-D8FCUJ6>
2020-05-20 13:07:52 +0530

    Bugs fixed

--- ITPM_Code_Complexity_Tool/Controllers/UploadController.cs
+++ ITPM_Code_Complexity_Tool/Controllers/UploadController.cs
@@ -28,8 +28,15 @@ namespace ITPM_Code_Complexity_Tool.Controllers
         [HttpPost]
         public ActionResult UploadFile(HttpPostedFileBase[] files)
         {
+            String REDIRECT_PAGE = "UploadFile";
+            var fi = files;
 
+            if( fi != null)
+            {
 
+            
+            
+            
             System.IO.DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/uploadedFiles/"));
 
             foreach (FileInfo file in di.GetFiles())
@@ -39,70 +46,86 @@ namespace ITPM_Code_Complexity_Tool.Controllers
 
             // Common FileNames object to use
             List<FileNames> fileNamesList = new List<FileNames>();
-            String REDIRECT_PAGE = "";
+           
 
 
 
             Unzipper unzipper = new Unzipper(fileNamesList);// Declaring common obj
 
-            foreach (var file in files)
-            {
-
-                if (file.FileName != null)
+                try
                 {
 
-                    string FILE_NAME = Path.GetFileName(file.FileName);
-                    string SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
-                    file.SaveAs(SAVE_PATH);
 
 
-                    // Checking whether if a zip file is being uploaded
-
-                    if (System.IO.Path.GetExtension(file.FileName) == ".zip") // Uploded file is a zip file
+                    foreach (var file in files)
                     {
 
-                        unzipper.setFileName(file.FileName);
-                        unzipper.Unzip();// Unzipping
+                        if (file.FileName != null)
+                        {
+
+                            string FILE_NAME = Path.GetFileName(file.FileName);
+                            string SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
+                            file.SaveAs(SAVE_PATH);
+
+
+                            // Checking whether if a zip file is being uploaded
+
+                            if (System.IO.Path.GetExtension(file.FileName) == ".zip") // Uploded file is a zip file
+                            {
+
+                                unzipper.setFileName(file.FileName);
+                                unzipper.Unzip();// Unzipping
 
 
 
-                        SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
+                                SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
+
+                            }
+
+
+
+                            if (System.IO.Path.GetExtension(FILE_NAME) != ".zip")
+                            {
+                                fileNamesList.Add(new FileNames(FILE_NAME)); // Add file name to the list
+                            }
+
+
+
+                        }
+
+
+
+                        if (fileNamesList.Count > 1)// Check how many file are being uploaded
+                        {
+                            REDIRECT_PAGE = "MultipleFiles";  // If multiple files are being uploaded, Redirect to this page
+                        }
+                        else if (fileNamesList.Count == 1)
+                        {
+                            REDIRECT_PAGE = "Tool_Home";
+                        }
+
+                        else // If no file is uploaded, 
+                        {
+                            REDIRECT_PAGE = "UploadFile";
+                        }
+
 
                     }
-
-
-
-                    if (System.IO.Path.GetExtension(FILE_NAME) != ".zip")
-                    {
-                        fileNamesList.Add(new FileNames(FILE_NAME)); // Add file name to the list
-                    }
-
-
-
                 }
-
-
-                if (fileNamesList.Count > 1)// Check how many file are being uploaded
+                catch(Exception e)
                 {
-                    REDIRECT_PAGE = "MultipleFiles";  // If multiple files are being uploaded, Redirect to this page
+                    return Redirect(REDIRECT_PAGE);
                 }
-                else if (fileNamesList.Count == 1)
-                {
-                    REDIRECT_PAGE = "Tool_Home";
-                }
-
-                else // If no file is uploaded, 
-                {
-                    REDIRECT_PAGE = "UploadFile";
-                }
-
-
-            }
+            
 
             TempData["UPLOADED_FILES_LIST"] = fileNamesList;
             TempData.Keep("UPLOADED_FILES_LIST");
             ViewBag.names = fileNamesList;
+
+
+            }
             return Redirect(REDIRECT_PAGE);
+           
 
         }
 
