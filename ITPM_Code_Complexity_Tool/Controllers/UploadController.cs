598007990f158902457714d0252ec30df6ed3488
Chamela Aluthgedara <chamelaaluthgedara12@gmail.com>
2020-05-22 06:25:53 +0530

    File upload bug fixed

--- ITPM_Code_Complexity_Tool/Controllers/UploadController.cs
+++ ITPM_Code_Complexity_Tool/Controllers/UploadController.cs
@@ -1,13 +1,13 @@
-﻿using ITPM_Code_Complexity_Tool.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Web;
+using System.IO;
 using System.Web.Mvc;
+using ITPM_Code_Complexity_Tool.Models;
 
 namespace ITPM_Code_Complexity_Tool.Controllers
 {
-   
     public class UploadController : Controller
     {
 
@@ -28,87 +28,104 @@ namespace ITPM_Code_Complexity_Tool.Controllers
         [HttpPost]
         public ActionResult UploadFile(HttpPostedFileBase[] files)
         {
+            String REDIRECT_PAGE = "UploadFile";
+            var fi = files;
 
-            if(Directory.Exists(Server.MapPath("~/uploadedFiles")) == false)
-            {
-                System.IO.Directory.CreateDirectory(Server.MapPath("~/uploadedFiles"));
-            }
-
-            System.IO.DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/uploadedFiles"));
-            foreach(FileInfo file in di.GetFiles())
-            {
-                file.Delete();
-            }
-
-
-
-
-            // Common FileNames object to use
-            List<FileNames> fileNamesList = new List<FileNames>();
-            String REDIRECT_PAGE = "";
-
-
-
-            Unzipper unzipper = new Unzipper(fileNamesList);// Declaring common obj
-
-            foreach (var file in files)
+            if (fi != null)
             {
 
-                if (file.FileName != null)
+
+
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/uploadedFiles/"));
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+
+                // Common FileNames object to use
+                List<FileNames> fileNamesList = new List<FileNames>();
+
+
+
+
+                Unzipper unzipper = new Unzipper(fileNamesList);// Declaring common obj
+
+                try
                 {
 
-                    string FILE_NAME = Path.GetFileName(file.FileName);
-                    string SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
-                    file.SaveAs(SAVE_PATH);
 
 
-                    // Checking whether if a zip file is being uploaded
-
-                    if (System.IO.Path.GetExtension(file.FileName) == ".zip") // Uploded file is a zip file
+                    foreach (var file in files)
                     {
 
-                        unzipper.setFileName(file.FileName);
-                        unzipper.Unzip();// Unzipping
+                        if (file.FileName != null)
+                        {
+
+                            string FILE_NAME = Path.GetFileName(file.FileName);
+                            string SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
+                            file.SaveAs(SAVE_PATH);
+
+
+                            // Checking whether if a zip file is being uploaded
+
+                            if (System.IO.Path.GetExtension(file.FileName) == ".zip") // Uploded file is a zip file
+                            {
+
+                                unzipper.setFileName(file.FileName);
+                                unzipper.Unzip();// Unzipping
 
 
 
-                        SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
+                                SAVE_PATH = Path.Combine(Server.MapPath("~/uploadedFiles"), FILE_NAME);
+
+                            }
+
+
+
+                            if (System.IO.Path.GetExtension(FILE_NAME) != ".zip")
+                            {
+                                fileNamesList.Add(new FileNames(FILE_NAME)); // Add file name to the list
+                            }
+
+
+
+                        }
+
+
+
+                        if (fileNamesList.Count > 1)// Check how many file are being uploaded
+                        {
+                            REDIRECT_PAGE = "MultipleFiles";  // If multiple files are being uploaded, Redirect to this page
+                        }
+                        else if (fileNamesList.Count == 1)
+                        {
+                            REDIRECT_PAGE = "Tool_Home";
+                        }
+
+                        else // If no file is uploaded, 
+                        {
+                            REDIRECT_PAGE = "UploadFile";
+                        }
+
 
                     }
-
-
-
-                    if (System.IO.Path.GetExtension(FILE_NAME) != ".zip")
-                    {
-                        fileNamesList.Add(new FileNames(FILE_NAME)); // Add file name to the list
-                    }
-
-
-
                 }
-
-
-                if (fileNamesList.Count > 1)// Check how many file are being uploaded
+                catch (Exception e)
                 {
-                    REDIRECT_PAGE = "MultipleFiles";  // If multiple files are being uploaded, Redirect to this page
-                }
-                else if (fileNamesList.Count == 1)
-                {
-                    REDIRECT_PAGE = "Tool_Home";
+                    return Redirect(REDIRECT_PAGE);
                 }
 
-                else // If no file is uploaded, 
-                {
-                    REDIRECT_PAGE = "UploadFile";
-                }
+
+                TempData["UPLOADED_FILES_LIST"] = fileNamesList;
+                TempData.Keep("UPLOADED_FILES_LIST");
+                ViewBag.names = fileNamesList;
 
 
             }
-
-            TempData["UPLOADED_FILES_LIST"] = fileNamesList;
-            TempData.Keep("UPLOADED_FILES_LIST");
-            ViewBag.names = fileNamesList;
             return Redirect(REDIRECT_PAGE);
+
 
         }
 
